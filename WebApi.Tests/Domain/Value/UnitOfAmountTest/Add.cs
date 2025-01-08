d5886321060226bddcc7a1502b4e8045193fe2dc
Taketo-Yoda <exhaust7@hotmail.com>
2025-01-08 19:17:33 +0900

    Add UnitOfAmount's test

--- WebApi.Tests/Domain/Value/UnitOfAmountTest/Add.cs
+++ WebApi.Tests/Domain/Value/UnitOfAmountTest/Add.cs
@@ -16,9 +16,11 @@ public class Add
     public void TestAddDecimalAtZeroVal()
     {
         UnitOfAmount uom = new(1);
+        UnitOfAmount bef = uom;
         UnitOfAmount ans = uom.Add(0);
 
         Assert.Equal(1, uom.Value);
+        Assert.Equal(bef, uom);
         Assert.Equal(1, ans.Value);
     }
 
@@ -26,10 +28,44 @@ public class Add
     public void TestAddDecimalAtOne()
     {
         UnitOfAmount uom = new(1);
+        UnitOfAmount bef = uom;
         UnitOfAmount ans = uom.Add(1);
 
         Assert.Equal(1, uom.Value);
+        Assert.Equal(bef, uom);
         Assert.Equal(2, ans.Value);
+    }
+
+    [Fact]
+    public void TestAddDecimalToResultMaximum001()
+    {
+        UnitOfAmount uom = new(998);
+        UnitOfAmount bef = uom;
+        UnitOfAmount ans = uom.Add(1);
+
+        Assert.Equal(998, uom.Value);
+        Assert.Equal(bef, uom);
+        Assert.Equal(999, ans.Value);
+    }
+
+    [Fact]
+    public void TestAddDecimalToResultMaximum002()
+    {
+        UnitOfAmount uom = new(998);
+        UnitOfAmount bef = uom;
+        UnitOfAmount ans = uom.Add(2);
+
+        Assert.Equal(998, uom.Value);
+        Assert.Equal(bef, uom);
+        Assert.Equal(1000, ans.Value);
+    }
+
+    [Fact]
+    public void TestAddDecimalToResultMaximum003()
+    {
+        UnitOfAmount uom = new(998);
+        var exception = Assert.Throws<InvalidOperationException>(() => uom.Add(3));
+        Assert.Equal("Value is too big", exception.Message);
     }
 
 }
