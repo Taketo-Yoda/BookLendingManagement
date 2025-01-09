b7c9660420391d66e8993070f14661c89f405405
Taketo-Yoda <exhaust7@hotmail.com>
2025-01-09 12:47:39 +0900

    UnitOfAmount.Addメソッドのテスト追加.

--- WebApi.Tests/Domain/Value/UnitOfAmountTest/Add.cs
+++ WebApi.Tests/Domain/Value/UnitOfAmountTest/Add.cs
@@ -24,6 +24,7 @@ public class Add
     {
         UnitOfAmount uom = new(1);
         UnitOfAmount bef = uom;
+
         UnitOfAmount ans = uom.Add(0);
 
         // イミュータブルであることの確認
@@ -38,6 +39,7 @@ public class Add
     {
         UnitOfAmount uom = new(1);
         UnitOfAmount bef = uom;
+
         UnitOfAmount ans = uom.Add(1);
 
         // イミュータブルであることの確認
@@ -52,6 +54,7 @@ public class Add
     {
         UnitOfAmount uom = new(998);
         UnitOfAmount bef = uom;
+
         UnitOfAmount ans = uom.Add(1);
 
         // イミュータブルであることの確認
@@ -66,6 +69,7 @@ public class Add
     {
         UnitOfAmount uom = new(998);
         UnitOfAmount bef = uom;
+
         UnitOfAmount ans = uom.Add(2);
 
         // イミュータブルであることの確認
@@ -88,6 +92,63 @@ public class Add
         // イミュータブルであることの確認
         Assert.Equal(998, uom.Value);
         Assert.Equal(bef, uom);
+    }
+
+    [Fact]
+    public void UnitOfAmount型の加算_和が境界値未満()
+    {
+        UnitOfAmount uom1 = new(998);
+        UnitOfAmount uom2 = new(1);
+        UnitOfAmount bef1 = uom1;
+        UnitOfAmount bef2 = uom2;
+
+        UnitOfAmount ans = uom1.Add(uom2);
+
+        // イミュータブルであることの確認
+        Assert.Equal(998, uom1.Value);
+        Assert.Equal(bef1, uom1);
+        Assert.Equal(1, uom2.Value);
+        Assert.Equal(bef2, uom2);
+        // 演算結果の確認
+        Assert.Equal(999, ans.Value);
+    }
+
+    [Fact]
+    public void UnitOfAmount型の加算_和が境界値と等価()
+    {
+        UnitOfAmount uom1 = new(998);
+        UnitOfAmount uom2 = new(2);
+        UnitOfAmount bef1 = uom1;
+        UnitOfAmount bef2 = uom2;
+
+        UnitOfAmount ans = uom1.Add(uom2);
+
+        // イミュータブルであることの確認
+        Assert.Equal(998, uom1.Value);
+        Assert.Equal(bef1, uom1);
+        Assert.Equal(2, uom2.Value);
+        Assert.Equal(bef2, uom2);
+        // 演算結果の確認
+        Assert.Equal(1000, ans.Value);
+    }
+
+    [Fact]
+    public void UnitOfAmount型の加算_和が境界値を超過()
+    {
+        UnitOfAmount uom1 = new(998);
+        UnitOfAmount uom2 = new(3);
+        UnitOfAmount bef1 = uom1;
+        UnitOfAmount bef2 = uom2;
+
+        // 例外の確認
+        var exception = Assert.Throws<InvalidOperationException>(() => uom1.Add(uom2));
+        // エラーメッセージの確認
+        Assert.Equal("Value is too big", exception.Message);
+        // イミュータブルであることの確認
+        Assert.Equal(998, uom1.Value);
+        Assert.Equal(bef1, uom1);
+        Assert.Equal(3, uom2.Value);
+        Assert.Equal(bef2, uom2);
     }
 
 }
